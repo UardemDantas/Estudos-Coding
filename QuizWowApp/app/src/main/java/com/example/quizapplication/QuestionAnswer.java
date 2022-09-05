60236998e57a26c191b5db772fe539ba5f347433
Uardem Dantas <uardemj12@gmail.com>
2022-09-05 13:31:19 -0300

    Exercicio Infnet.

---
+++ QuizWowApp/app/src/main/java/com/example/quizapplication/QuestionAnswer.java
@@ -0,0 +1,23 @@
+package com.example.quizapplication;
+
+public class QuestionAnswer {
+
+    public static String question[]={
+            "Quem é o atual rei da Aliança ?",
+            "Quem foi o vilao originado da Horda?",
+            "Qual é o nome da mulher do MilHouse?"
+    };
+
+    public static String choices[][] = {
+            {"Bolvar", "Varian", "Anduin", "Turalyon"},
+            {"Arthas","Illidan", "Garrosh", "BlackHand"},
+            {"Malevola", "Milevola", "Melevola", "Cristina"}
+    };
+
+    public static String correctAnswers[]={
+        "Turalyon",
+        "Garrosh",
+        "Milevola"
+    };
+
+}
