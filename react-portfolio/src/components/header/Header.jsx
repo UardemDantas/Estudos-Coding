6dcba48e16b23f880138e169750c9a727357f1b9
Uardem Dantas <uardemj12@gmail.com>
2022-09-06 17:58:28 -0300

    First Step Portfolio

---
+++ react-portfolio/src/components/header/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './header.css'
+
+const Header = () => {
+    return (
+        <header>
+            <div className="container header_container">
+            <h5>Hello I'm</h5>
+            <h1>Uardem Junior</h1>
+            <h5 className="text-light"> Front-End Developer</h5>
+            
+            </div>
+        </header>
+    )
+}
+
+export default Header
